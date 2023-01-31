a20cbf4e111c0adb072eed65451d889ea74f49bc
mroobert <robertmihairo94@gmail.com>
2023-01-31 14:32:41 +0200

    test: add other method test

--- http/server_test.go
+++ http/server_test.go
@@ -14,3 +14,14 @@ func Test_SomeMethod(t *testing.T) {
 		t.Fatal("Invalid method test")
 	}
 }
+
+func Test_OtherMethod(t *testing.T) {
+	t.Parallel()
+
+	want := true
+
+	got := true
+	if got != want {
+		t.Fatal("Invalid other method test")
+	}
+}
