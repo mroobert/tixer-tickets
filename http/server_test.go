ad545af07080634496ffdde960c4ec8100e69435
mroobert <robertmihairo94@gmail.com>
2023-01-31 14:24:47 +0200

    test: add demo test

---
+++ http/server_test.go
@@ -0,0 +1,16 @@
+package http_test
+
+import (
+	"testing"
+)
+
+func Test_SomeMethod(t *testing.T) {
+	t.Parallel()
+
+	want := true
+
+	got := true
+	if got != want {
+		t.Fatal("Invalid method")
+	}
+}
